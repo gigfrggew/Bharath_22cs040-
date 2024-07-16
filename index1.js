17776d922757331601668dafccd9e9375cea8864
Bharath B Shetty <shettybharath004@gmail.com>
2024-07-16 21:58:16 +0530

    my second commit

---
+++ index1.js
@@ -0,0 +1,15 @@
+function findMax(numbers) {
+    if (numbers.length === 0) {
+        return null;
+    }
+    let max = numbers[0];
+    for (let i = 1; i < numbers.length; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    return max;
+}
+
+console.log(findMax([3, 5, 7, 2, 8])); 
+console.log(findMax([]));
